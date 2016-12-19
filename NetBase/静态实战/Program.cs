f3e09ef69647c5940534f88760ab469ad0dcdaf4
yuanhuaming <yuanhuaming@taolx.com>
2016-12-19 10:49:40 +0800

    代码测试工具

---
+++ NetBase/静态实战/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 类型转换1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            foreach (var VARIABLE in Task)
+            {
+                task
+            }
+
+        }
+
+        private static string  contents="hello world!!!!   ";
+
+
+        public static void SayHi(string name)
+        {
+            Console.WriteLine($"{contents} + + {name}");
+        }
+    }
+
+
+
+}
