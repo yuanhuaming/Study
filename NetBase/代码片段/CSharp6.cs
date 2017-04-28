5f8eafb15dbe9bf80fd1b7ad2c023bb108f54de4
yuanhuaming <yuanhuaming@taolx.com>
2017-04-28 10:58:27 +0800

    自动更新

---
+++ NetBase/代码片段/CSharp6.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 代码片段
+{
+    /// <summary>
+    ///  CSharp6新语法
+    /// </summary>
+    class CSharp6
+    {
+
+    }
+
+
+    /// <summary>
+    /// Struct的默认构造函数
+    /// </summary>
+    struct Complex1
+    {
+        public string Re { get; set; }
+        public int Im { get; set; }
+
+    }
+ 
+
+    /// <summary>
+    /// 方法表达式
+    /// </summary>
+    public class UserInfo2
+    {
+        public string Name { get; set; } = "Tom";
+        public int Age { get; set; } = 12;
+
+
+        public int Height { get { return 168 + new Random().Next(1, 10); } }
+        public int HeightNew => 168 + new Random().Next(1, 10);
+
+        public void Introduce()
+        {
+            //C#6 字符串拼接方式，很方便吧！
+            Console.WriteLine($"my name is {Name},{Age}.");
+        }
+
+        public void IntroduceNew() => Console.WriteLine($"my name is {Name},{Age}.");
+
+
+
+        #region 空引用检查
+        public void SayHello(UserInfo2 user)
+        {
+            if (user.Name == null)
+                return;
+            Console.WriteLine($"hi, {user.Name}");
+        }
+        //输出 hi,  
+        public void SayHelloNew(UserInfo2 user)
+        {
+            Console.WriteLine($"hi, {user?.Name}");
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 获取参数/变量的名称
+        /// 原先要用反射获取
+        /// </summary>
+        public static void GetObjectName()
+        {
+            Console.WriteLine(nameof(UserInfo2));
+        }
+    }
+
+ 
+
+
+
+}
---
+++ NetBase/代码片段/ClickOne更新.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Application;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 代码片段
+{
+
+    public class Update
+    {
+
+
+
+
+        private void InstallUpdateSyncWithInfo2()
+        {
+            if (ApplicationDeployment.IsNetworkDeployed == true)
+            {
+                ApplicationDeployment thisDeployment = ApplicationDeployment.CurrentDeployment;
+                //this.Text = "正在检测更新...";
+                if (thisDeployment.CheckForUpdate() == true)
+                {
+                    if (MessageBox.Show("检测到有新的版本可以进行更新,现在需要更新吗?", "选择是否要进行更新", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                    {
+                       // this.Text = "正在更新中";
+                        thisDeployment.Update();
+                        MessageBox.Show("更新完毕,将要重启程序!");
+                        Application.Restart();
+                    }
+                    else
+                    {
+                      //  this.Text = Application.ProductName + " " + Application.ProductVersion;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("并没有新的版本进行更新!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("这不是网络发布的程序");
+            }
+        }
+        private void InstallUpdateSyncWithInfo()
+        {
+            UpdateCheckInfo info = null;
+
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+
+                try
+                {
+                    info = ad.CheckForDetailedUpdate();
+
+                }
+                catch (DeploymentDownloadException dde)
+                {
+                    MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
+                    return;
+                }
+                catch (InvalidDeploymentException ide)
+                {
+                    MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
+                    return;
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
+                    return;
+                }
+
+                if (info.UpdateAvailable)
+                {
+                    Boolean doUpdate = true;
+
+                    if (!info.IsUpdateRequired)
+                    {
+                        DialogResult dr = MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", MessageBoxButtons.OKCancel);
+                        if (!(DialogResult.OK == dr))
+                        {
+                            doUpdate = false;
+                        }
+                    }
+                    else
+                    {
+                        // Display a message that the app MUST reboot. Display the minimum required version.  
+                        MessageBox.Show("This application has detected a mandatory update from your current " +
+                            "version to version " + info.MinimumRequiredVersion.ToString() +
+                            ". The application will now install the update and restart.",
+                            "Update Available", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                    }
+
+                    if (doUpdate)
+                    {
+                        try
+                        {
+                            ad.Update();
+                            MessageBox.Show("The application has been upgraded, and will now restart.");
+                            Application.Restart();
+                        }
+                        catch (DeploymentDownloadException dde)
+                        {
+                            MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+}
